26b34e9a986eeef8c3e9025b6db428e45f976db0
aw10g24 <aw10g24@JFB36-LVL05-W07>
2024-11-26 13:20:02 +0000

    Started Rock, Paper, Scissors game

---
+++ Rock Paper Scissors/Rock Paper Scissors/Rock_Paper_Scissors.py
@@ -0,0 +1,27 @@
+from tkinter import END
+
+
+def haveTurn(Player):
+    print("Player" + Player)
+    print("1. Rock")
+    print("2. Paper")
+    print("3. Scissors")
+    playerChoice = input("Select your choice: ")
+    return playerChoice
+
+def CheckResults(choiceOne,choiceTwo):
+    if(choiceOne == choiceTwo):
+        print("Draw!")
+        return 0
+
+inGame = True
+while(inGame):
+    One = haveTurn("1")
+    Two = haveTurn("2")
+    CheckResults(One, Two)
+    NewGame = input("Play again? (Y/N) ")
+    if(NewGame == "N"):
+        inGame = False
+        
+
+
